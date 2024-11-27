138a63265ae118d7bb7a782419defd65e88e792d
Gardier S. <72516749+Neitsabeski@users.noreply.github.com>
2024-11-27 15:55:49 +0100

    Update batch.bat

--- src/batch.bat
+++ src/batch.bat
@@ -1,2 +1,10 @@
 @echo off
+
+:: Lire le chemin de python.exe depuis le fichier python_path.txt
+set /p PYTHON_PATH=<..\python_path.txt
+
+:: Exécuter le script Python avec le chemin récupéré
+start "" "%PYTHON_PATH%" ".\PDF2EXCEL_ceta.py"
+
+
 start "" "C:\Python312\python.exe" ".\PDF2EXCEL_ceta.py"
